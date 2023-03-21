98593d738dff7cf5db60fda61de2cefceb0ac54a
suhas <suhas@plainsurf.com>
2023-03-21 11:19:51 +0530

    use of useLocation hook for dyanamic active class for active pages and added a static form on home pahe

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -1,25 +1,8 @@
-import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Harry",
-        "class": "5b"
-    }
-
-    const [state, setState] = useState(s1);
-
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Larry",
-                "class": "10b"
-            })
-        }, 1000)
-    }
-
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
